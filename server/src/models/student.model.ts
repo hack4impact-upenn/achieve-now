0f2e7a2c94b6e91a87ff40e813b1e48343ebb99d
Caroline Chen <64424139+carolineychen8@users.noreply.github.com>
2023-10-01 14:32:03 -0400

    Update student.model.ts

--- server/src/models/student.model.ts
+++ server/src/models/student.model.ts
@@ -14,8 +14,17 @@ const StudentSchema = new mongoose.Schema({
     type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'School' }],
     required: true,
   },
+  teacher_id: {
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    required: true,
+  },
   coach_id: {
     type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    required: true,
+  },
+  block_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Block',
     required: true,
   },
   lesson_level: {
@@ -65,12 +74,36 @@ const StudentSchema = new mongoose.Schema({
     type: String,
     required: false,
   },
+  parent_name: {
+    type: String,
+    required: true,
+  },
+  parent_commmunication_days: {
+    type: String,
+    enum: ['weekends', 'weekdays', 'any'],
+    required: false,
+  },
+  parent_communication_times: {
+    type: String,
+    enum: ['morning', 'afternoon', 'evening'],
+    required: false,
+  },
+  media_waiver: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  work_habits: {
+    type: String,
+    required: false,
+  },
 });
 
 interface IStudent extends mongoose.Document {
   _id: string;
   user_id: string;
   school_id: [string];
+  teacher_id: [string];
   coach_id: [string];
   block_id: string;
   lesson_level: string;
