5f53f295f7a2d079fe4cae18d579c674671bcb37
Caroline Chen <64424139+carolineychen8@users.noreply.github.com>
2023-11-12 14:58:57 -0500

    adding components

--- client/src/TeacherDashboard.tsx
+++ client/src/TeacherDashboard.tsx
@@ -1,51 +1,89 @@
-/* eslint-disable camelcase */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
+import Card from '@mui/material/Card';
+import Button from '@mui/material/Button';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 import { styled } from '@mui/system';
 import { useNavigate, useParams } from 'react-router-dom';
 // eslint-disable-next-line
 import { useData } from './util/api';
-import StudentCard from './components/buttons/StudentCard';
+import { StudentCardFromID } from './Admin/StudentCard';
 import PageHeader from './components/PageHeader';
 import PhoneticsTable from './components/buttons/PhoneticsTable';
+import { Grid } from '@mui/material';
+import LessonLevels from './components/LessonLevels';
 
 const ScrollableBox = styled(Box)({
   overflowY: 'auto',
   maxHeight: '100%',
 });
 
-// // eslint-disable-next-line
-// function createStudentCard(student: any) {
-//   const id = student.user_id;
-//   console.log(id)
-//   const user = useData(`users/${id}`);
-//   console.log(user)
-//   if (user) {
-//     const info = user.data;
-//     const name = `${info.firstName} ${info.lastName}`;
-//     const lesson = student.lesson_level;
-//     return <StudentCard studentName={name} lesson={lesson} />;
-//   }
-// }
-
 // eslint-disable-next-line
 function createData(data: any) {
   return data.map((student: any) => {
-    return <StudentCard studentID={student.user_id} lesson="Lesson 1" />;
+    return <StudentCardFromID studentID={student.user_id} lesson="Lesson 1" />;
   });
+}
+function StudentName(props: any) {
+  const { id } = props;
+  const user = useData(`user/${id}`);
+  return (
+    <Typography color="text-primary" sx={{ fontSize: 20, color: 'black' }}>
+      {user?.data.firstName} {user?.data.lastName}
+    </Typography>
+  );
+}
+function StudentConcernsCard(props: any) {
+  const { students, title, description } = props;
+  const [showMore, setShowMore] = useState(false);
+  return (
+    <Card sx={{ marginBotom: '30px' }}>
+      <CardContent sx={{ marginBotom: '30px' }}>
+        <Typography
+          color="text-primary"
+          sx={{ fontSize: 30, fontWeight: 'bold' }}
+        >
+          {title}
+        </Typography>
+        <Typography color="text-secondary" sx={{ fontSize: 17 }}>
+          {description}
+        </Typography>
+        <div style={{ marginTop: '10px' }}>
+          {students
+            .slice(0, showMore ? students.length : 3)
+            .map((student: any) => {
+              return <StudentName id={student.user_id} />;
+            })}
+        </div>
+        {students.length > 3 && (
+          <Button
+            onClick={() => setShowMore(!showMore)}
+            variant="contained"
+            sx={{ marginTop: '10px' }}
+          >
+            {' '}
+            {showMore ? 'Show Less' : 'Show More'}
+          </Button>
+        )}
+      </CardContent>
+    </Card>
+  );
 }
 
 function SplitGrid() {
   const id = '111';
-  const students = useData(`student/teacher/${id}`);
-  const studentData = students?.data ?? [];
+  // const students = useData(`student/teacher/${id}`);
+  // const studentData = students?.data ?? [];
 
   const { teacherID } = useParams();
-  let final_id = '';
+  let final_id = '111';
   if (teacherID) {
     final_id = teacherID;
   }
+  const students = useData(`student/teacher/${final_id}`);
+  const studentData = students?.data ?? [];
 
   // const student_users = []
   // for (let i = 0; i < studentData.length; i++) {
@@ -53,6 +91,13 @@ function SplitGrid() {
   //   const user = useData(`users/${id}`);
   //   student_users.push(user);
   // }
+  const academicFlags = studentData.filter(
+    (student: any) => student.progressFlag,
+  );
+  const attendanceFlags = studentData.filter(
+    (student: any) => student.attendanceFlag,
+  );
+
   return (
     <Box>
       <PageHeader />
@@ -86,7 +131,32 @@ function SplitGrid() {
             square
           >
             <h2>Class Progress</h2>
-            <PhoneticsTable teacherID={final_id} />
+            <Grid item xs={8}>
+              {academicFlags.length > 0 && ( // TODO: Passed in teacher ID
+                <StudentConcernsCard
+                  students={academicFlags}
+                  title="Academic Concerns"
+                  description="Students with a poor academic progress pattern"
+                />
+              )}
+            </Grid>
+            <Grid item xs={8}>
+              {attendanceFlags.length > 0 && (
+                <div style={{ marginTop: '10px' }}>
+                  <StudentConcernsCard
+                    students={attendanceFlags}
+                    title="Attendance Concerns"
+                    description="Students with a poor attendance pattern"
+                  />
+                </div>
+              )}
+            </Grid>
+            <Grid item xs={8}>
+              <PhoneticsTable teacherID={final_id} />
+            </Grid>
+            <Grid item xs={8}>
+              <LessonLevels/>
+            </Grid>
           </Paper>
         </Box>
       </Box>
