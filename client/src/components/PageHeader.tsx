c558c31317964598c3103690bb22155b5058e74a
Roshan Bellary <roshan.bellary@gmail.com>
2023-11-19 15:42:00 -0500

    Updated Admin Routing in the Header and added back button functionality. Task not fully finished yet

--- client/src/components/PageHeader.tsx
+++ client/src/components/PageHeader.tsx
@@ -5,20 +5,52 @@ import AppBar from '@mui/material/AppBar';
 import Button from '@mui/material/Button';
 import { Box } from '@mui/system';
 
+import { ArrowBack } from '@mui/icons-material';
 import COLORS from '../assets/colors';
 import Logo from '../assets/achieve-now-logo.png';
 
 import { useAppDispatch, useAppSelector } from '../util/redux/hooks';
 import { selectUser, logout as logoutAction } from '../util/redux/userSlice';
-import { logout as logoutApi } from '../Home/api';
+import { logout, logout as logoutApi } from '../Home/api';
 
 export default function Header() {
   const navigator = useNavigate();
-
-  const handleLogin = () => {
-    navigator('/login');
+  const dispatch = useAppDispatch();
+  const location = useLocation();
+  const handleLogout = async () => {
+    if (await logoutApi()) {
+      logout();
+      navigator('/login', { replace: true });
+    }
   };
-
+  const user = useAppSelector(selectUser);
+  console.log(user);
+  const path = location.pathname;
+  const handleAdminHome = () => {
+    navigator('/admin-menu');
+  };
+  const handleAdminSessions = () => {
+    navigator('/admin-sessions');
+  };
+  const handleAdminAttendance = () => {
+    navigator('/admin-attendance');
+  };
+  const handleAdminProfiles = () => {
+    navigator('/admin-profiles');
+  };
+  const handleAdminCurriculum = () => {
+    navigator('/admin-curriculum');
+  };
+  const handleBackButton = () => {
+    navigator(-1);
+  };
+  const noBackButton = [
+    '/admin-sessions',
+    '/admin-attendance',
+    '/admin-profiles',
+    '/admin-curriculum',
+    '/admin-menu',
+  ];
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar
@@ -32,15 +64,110 @@ export default function Header() {
           justifyContent: 'space-between',
         }}
       >
-        <img alt="achieve now logo" src={Logo} width={95} height={44} />
-        <Button
-          sx={{ color: 'white', borderColor: 'white' }}
-          variant="outlined"
-          color="primary"
-          onClick={handleLogin}
+        <div>
+          <Button onClick={handleAdminHome}>
+            <img alt="achieve now logo" src={Logo} width={95} height={44} />
+          </Button>
+          {!noBackButton.includes(path) ? (
+            <Button
+              sx={{ color: 'white', borderColor: 'white', marginRight: '10px' }}
+              variant="outlined"
+              color="primary"
+              onClick={handleBackButton}
+            >
+              <ArrowBack />
+            </Button>
+          ) : (
+            <div />
+          )}
+        </div>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            marginTop: 'auto',
+            marginBottom: 'auto',
+          }}
         >
-          Log Out
-        </Button>
+          {user && user.role === 'admin' ? (
+            <div>
+              {path !== '/admin-sessions' ? (
+                <Button
+                  sx={{
+                    color: 'white',
+                    borderColor: 'white',
+                    marginRight: '10px',
+                  }}
+                  variant="outlined"
+                  color="primary"
+                  onClick={handleAdminSessions}
+                >
+                  Sessions
+                </Button>
+              ) : (
+                false
+              )}
+              {path !== '/admin-attendance' ? (
+                <Button
+                  sx={{
+                    color: 'white',
+                    borderColor: 'white',
+                    marginRight: '10px',
+                  }}
+                  variant="outlined"
+                  color="primary"
+                  onClick={handleAdminAttendance}
+                >
+                  Attendance
+                </Button>
+              ) : (
+                false
+              )}
+              {path !== '/admin-profiles' ? (
+                <Button
+                  sx={{
+                    color: 'white',
+                    borderColor: 'white',
+                    marginRight: '10px',
+                  }}
+                  variant="outlined"
+                  color="primary"
+                  onClick={handleAdminProfiles}
+                >
+                  Profiles
+                </Button>
+              ) : (
+                false
+              )}
+              {path !== '/admin-curriculum' ? (
+                <Button
+                  sx={{
+                    color: 'white',
+                    borderColor: 'white',
+                    marginRight: '10px',
+                  }}
+                  variant="outlined"
+                  color="primary"
+                  onClick={handleAdminCurriculum}
+                >
+                  Curriculum
+                </Button>
+              ) : (
+                false
+              )}
+            </div>
+          ) : (
+            false
+          )}
+          <Button
+            sx={{ color: 'white', borderColor: 'white', marginRight: '10px' }}
+            variant="outlined"
+            color="primary"
+            onClick={handleLogout}
+          >
+            Log Out
+          </Button>
+        </div>
       </AppBar>
     </Box>
   );
